306b909463b26a72b59c966abc247cb9f671b38b
Ladan Zahir <ladan@Administrators-MacBook-Pro.local>
2011-06-29 15:37:13 -0400

    c

---
+++ NativeEarth/iphone/NativeEarthAppDelegate_iPhone.m
@@ -0,0 +1,66 @@
+//
+//  NativeEarthAppDelegate_iPhone.m
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-05-20.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NativeEarthAppDelegate_iPhone.h"
+
+@implementation NativeEarthAppDelegate_iPhone
+
+@synthesize viewController;
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [super application: application didFinishLaunchingWithOptions:launchOptions];
+    
+ 
+    self.viewController.internetConnectionStatus = self.internetConnectionStatus;
+    self.viewController.wifiConnectionStatus = self.wifiConnectionStatus;
+    self.viewController.remoteHostStatus = self.wifiConnectionStatus;
+    self.viewController.managedObjectContext = self.managedObjectContext;
+   
+  
+    [self.window addSubview:self.viewController.view];
+    [self.window makeKeyAndVisible];
+    return  YES;
+}
+
+- (void)dealloc
+{
+    
+  //  [self.viewController release];
+	[super dealloc];
+}
+- (void)reachabilityChanged:(NSNotification *)note {
+    Reachability* curReach = [note object];
+	NSParameterAssert([curReach isKindOfClass: [Reachability class]]);
+	[self updateStatusesWithReachability: curReach];
+}
+- (void) updateStatusesWithReachability: (Reachability*) curReach
+{
+    if(curReach == hostReach)
+	{
+        self.remoteHostStatus = [curReach currentReachabilityStatus];
+       // self.viewController.remoteHostStatus =self.remoteHostStatus;
+    }
+    
+	if(curReach == internetReach)
+	{	
+		
+        self.internetConnectionStatus= [curReach currentReachabilityStatus];
+     //   self.viewController.internetConnectionStatus =self.internetConnectionStatus;
+	}
+	if(curReach == wifiReach)
+	{
+        self.wifiConnectionStatus =[curReach currentReachabilityStatus];
+      //  self.viewController.wifiConnectionStatus=self.wifiConnectionStatus;
+	}
+	
+}
+
+
+@end
