5364d192d3772fc2c02d0733a95b8d0e0a0db13b
Ladan Zahir <ladan@Administrators-MacBook-Pro.local>
2011-06-29 13:36:12 -0400

    Initial Commit

---
+++ NativeEarth/NativeEarthAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  NativeEarthAppDelegate.h
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-06-29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NativeEarthAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ NativeEarth/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-06-29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
