ff86fe6b5e1a18a782c5b8b5d209173496f45eec
Ladan Zahir <ladan@Administrators-MacBook-Pro.local>
2011-06-29 15:55:46 -0400

    c

---
+++ NativeEarth/iphone/Locator/LocatorRootViewController-iPhone.h
@@ -0,0 +1,16 @@
+//
+//  LocatorRootViewController-iPhone.h
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-06-29.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LocatorRootViewController_iPhone : UITableViewController {
+    
+}
+
+@end
