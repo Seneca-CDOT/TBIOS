4e4ada605bdf8aa62023193de0fb531561a6bc6e
Ladan Zahir <ladan@Administrators-MacBook-Pro.local>
2011-07-08 17:40:40 -0400

    c

---
+++ NativeEarth/iphone/TripPlanner/TextViewCell.h
@@ -0,0 +1,35 @@
+//
+//  TextViewCell.h
+//  PTLog
+//
+//  Created by Ellen Miner on 2/20/09.
+//  Copyright 2009 RaddOnline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@class TextViewCell;  
+
+@protocol TextViewCellDelegate <NSObject>  
+
+// we will make one function mandatory to include  
+- (void)editDidFinish:(NSMutableDictionary *)result;  
+
+@optional  
+// and the other one is optional (this function has not been used in this tutorial)  
+- (void)editStarted:(UITextView *)field;  
+
+@end  
+// cell identifier for this custom cell
+extern NSString *kCellTextView_ID;
+
+@interface TextViewCell : UITableViewCell<UITextViewDelegate> {
+	IBOutlet UITextView *textView;
+     id <TextViewCellDelegate> delegate;  
+}
++ (TextViewCell*) createNewTextCellFromNib;
+
+@property (nonatomic, retain) UITextView *textView;
+@property (nonatomic, assign) id <TextViewCellDelegate> delegate;  
+@end
