637c93b66c389af833238291d40f1a1e80b39ff3
Ladan Zahir <ladan@Administrators-MacBook-Pro.local>
2011-07-08 17:41:18 -0400

    c

---
+++ NativeEarth/iphone/TripPlanner/VisitPlannerRootViewController_iPhone.h
@@ -0,0 +1,16 @@
+//
+//  VisitPlannerRootViewController_iPhone.h
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-07-06.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseTableViewController.h"
+
+@interface VisitPlannerRootViewController_iPhone : BaseTableViewController {
+    
+}
+
+@end
