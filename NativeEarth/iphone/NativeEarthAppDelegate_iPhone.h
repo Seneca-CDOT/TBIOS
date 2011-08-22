3661e3947102b614649080c5ca06bdcd6b34a0cc
Ladan Zahiroleslam <ladanzahir@gmail.com>
2011-08-22 12:34:10 -0400

    c

--- NativeEarth/iphone/NativeEarthAppDelegate_iPhone.h
+++ NativeEarth/iphone/NativeEarthAppDelegate_iPhone.h
@@ -10,23 +10,17 @@
 #import "NativeEarthAppDelegate.h"
 #import "LauncherViewController_iPhone.h"
 #import "NetworkDataGetter.h"
-
-@interface NativeEarthAppDelegate_iPhone :NativeEarthAppDelegate <NetworkDataGetterDelegate>{
+#import "LandGetter.h"
+@interface NativeEarthAppDelegate_iPhone :NativeEarthAppDelegate {
  
-  
-    BOOL updateCheckStarted;
-    BOOL updateCheckFinished;
-    NSMutableArray *  addArray ;
-    NSMutableArray *  deleteArray ;
-    NSMutableArray *  updateArray ;
-  //  LauncherViewController_iPhone *viewController;
+
 }
+@property (nonatomic , retain) LandGetter * landGetter;
 @property (nonatomic, retain) NSMutableArray *  updateArray ;
 @property (nonatomic, retain) IBOutlet LauncherViewController_iPhone * viewController;
 
 - (void) updateStatusesWithReachability: (Reachability*) curReach;
 
--(void) GetLandShortsFromWebService;
--(void) GetFirstNationLandFromWebServiceWithLandID:(NSNumber *)landID;
+
 @end
 
--- NativeEarth/iphone/NativeEarthAppDelegate_iPhone.m
+++ NativeEarth/iphone/NativeEarthAppDelegate_iPhone.m
@@ -8,26 +8,13 @@
 
 #import "NativeEarthAppDelegate_iPhone.h"
 
-#import "LandShortArray.h"
-#import "LandShort.h"
-#import "LocalLandGetter.h"
-#import "WSLand.h"
+#import "LandGetter.h"
 
-NSInteger firstNumSort(id str1, id str2, void *context) {
-    int num1 = [str1 integerValue];
-    int num2 = [str2 integerValue];
-    
-    if (num1 < num2)
-        return NSOrderedAscending;
-    else if (num1 > num2)
-        return NSOrderedDescending;
-    
-    return NSOrderedSame;
-}
+
 
 @implementation NativeEarthAppDelegate_iPhone
 
-@synthesize viewController,updateArray;
+@synthesize viewController,updateArray,landGetter;
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -40,10 +27,8 @@ NSInteger firstNumSort(id str1, id str2, void *context) {
     self.viewController.wifiConnectionStatus = self.wifiConnectionStatus;
     self.viewController.remoteHostStatus = self.wifiConnectionStatus;
     self.viewController.managedObjectContext = self.managedObjectContext;
-   
-         updateCheckStarted=FALSE;
-      updateCheckFinished=FALSE;
-  
+    self.landGetter = [[LandGetter alloc] initWithManagedObjectContext:self.managedObjectContext];
+
     [self.window addSubview:self.viewController.view];
     [self.window makeKeyAndVisible];
     return  YES;
@@ -51,6 +36,8 @@ NSInteger firstNumSort(id str1, id str2, void *context) {
 
 - (void)dealloc
 {
+    [landGetter release];
+    [updateArray release];
     
   //  [self.viewController release];
 	[super dealloc];
@@ -80,123 +67,10 @@ NSInteger firstNumSort(id str1, id str2, void *context) {
        // self.viewController.wifiConnectionStatus=self.wifiConnectionStatus;
 	}
 	
-    //check for updates
-    if (!updateCheckStarted) {
-    
-    addArray  =[[NSMutableArray alloc] init];
-    deleteArray  =[[NSMutableArray alloc] init];
-    self.updateArray  =[[NSMutableArray alloc] init];
-    if (remoteHostStatus!=NotReachable) {
-            [self GetLandShortsFromWebService];
-        }
-        updateCheckStarted = YES;
-    }
-    
+        
 }
 
--(void) GetLandShortsFromWebService{
-    NSString *url = @"http://localhost/~ladan/FirstNationList";
-    NetworkDataGetter * dataGetter = [[NetworkDataGetter alloc]init];
-    dataGetter.delegate = self;
-    
-    // Reference the app's network activity indicator in the status bar
-	[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    [dataGetter GetResultsFromUrl:url];
-}
--(void) GetFirstNationLandFromWebServiceWithLandID:(NSNumber *)landID{
-    //pass landID and language here:
-    
-    NSString *url = @"http://localhost/~ladan/Algonquin ";
-    NetworkDataGetter * dataGetter = [[NetworkDataGetter alloc]init];
-    dataGetter.delegate = self;
-    
-    // Reference the app's network activity indicator in the status bar
-	[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    [dataGetter GetResultsFromUrl:url];
-    
-}
 
-#pragma mark - NetworkDataGetterDelegate Methods
-
--(void)DataUpdate:(id)object{
-    if(!updateCheckFinished){// check for updates
-        //first get the local dictionary 
-        LandShortDictionary * localLandShortDict = [[LandShortDictionary alloc] initWithManagedObjectContext:self.managedObjectContext];
-
-        //get the largest local land id 
-        NSArray * localKeyArray= [localLandShortDict allKeys];
-        int localLargestID = [[localKeyArray lastObject] intValue];
-   
-        //second get the network array:
-        NSDictionary * networkDict = (NSDictionary* )object;
-        NSArray * networkIDArray =[[networkDict allKeys] sortedArrayUsingFunction: firstNumSort context:NULL ];
-    
-        int netWorkLargestID =[[networkIDArray lastObject] intValue];
-    
-        // get the maximum of largest ids
-        int max = MAX(localLargestID, netWorkLargestID);
-        
-        // figure out additiond, deletions and updates
-        for (int i=0 ; i<=max; i++) {
-            BOOL landExistLocally = [localKeyArray containsObject: [NSString stringWithFormat:@"%d",i]];
-            BOOL landExistRemothly =[networkIDArray containsObject:[NSString stringWithFormat:@"%d",i]];
-        
-            if(landExistLocally && landExistRemothly){
-                LandShort *l = (LandShort*)[localLandShortDict valueForKey:[NSString stringWithFormat:@"%d",i]];
-                NSNumber  * localVersion=l.versionIdentifier;
-                NSNumber * remotVersion=[[networkDict valueForKey:[NSString stringWithFormat:@"%d",i]] valueForKey:@"VersionIdentifier"];
-                if ([remotVersion intValue]!=[localVersion intValue]) {
-                    [self.updateArray addObject:[NSNumber numberWithInt:i]];
-                }
-            }else if(landExistLocally){ // it should be deleted
-                [deleteArray addObject:[NSNumber numberWithInt:i]];
-            }else if (landExistRemothly){
-                [addArray addObject:[NSNumber numberWithInt:i]];
-            }
-        
-        }// end of for
-        //apply deletes:
-        if ([deleteArray count]>0) {
-            LocalLandGetter * localLandGetter =[[LocalLandGetter alloc] initWithManagedObjectContext:self.managedObjectContext];
-            for (NSNumber * n in deleteArray) {
-                Land * land = [localLandGetter GetLandWithLandID:[n intValue]];
-                [self.managedObjectContext deleteObject:land];
-            }
-            NSError * error;
-            if(![self.managedObjectContext save:&error]){
-            }
-        } // end of deletion
-        
-        //post update notification (there is listener in location info view)
-        if ([updateArray count]>0) {
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"UpdateArrayNotification" object:updateArray];        
-        }
-        //get objects to be addes
-
-        for (NSNumber * n in addArray){
-            [self GetFirstNationLandFromWebServiceWithLandID:n];
-        }
-        updateCheckFinished=YES;
-    
-    }else{// apply the additions
-  
-        WSLand * land = [[WSLand alloc] initWithDictionary:(NSDictionary*)object];
-        
-        Land * managedLand = [land ToManagedLand:self.managedObjectContext];
-
-        NSError * error;
-        if(![managedLand.managedObjectContext save:&error]){
-            NSLog(@"%@",[error description]);
-        } 
-    }
-    
-}
--(void)DataError:(NSError *)error{
-    // Reference the app's network activity indicator in the status bar
-	[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-	
-	NSLog(@"%@", [error description]);
-}
 
 
 
