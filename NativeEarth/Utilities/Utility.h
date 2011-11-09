494cf90077a7a5fb5f07a3c3708e8f2e5060f982
Ladan Zahiroleslam <ladanzahir@gmail.com>
2011-11-09 12:28:08 -0500

    c

--- NativeEarth/Utilities/Utility.h
+++ NativeEarth/Utilities/Utility.h
@@ -23,30 +23,6 @@
 
 static CLLocationCoordinate2D * parseCoordinatesStringAsCLLocationCoordinate2D(NSString * coordinates, NSUInteger * Coords2DLengthOut){
     
-   /* coordinates = [coordinates stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    NSArray* pointStrings = [coordinates  componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-    
-    CLLocationCoordinate2D * coords= NULL;
-    
-    for(int i = 0; i < pointStrings.count; i++)
-    {
-        // break the string down even further to latitude and longitude fields. 
-        NSString* currentPointString = [pointStrings objectAtIndex:i];
-        NSArray* latLonArr = [currentPointString componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@","]];
-        
-        CLLocationDegrees lng  = [[latLonArr objectAtIndex:0] doubleValue];
-        
-        CLLocationDegrees lat = [[latLonArr objectAtIndex:1] doubleValue];
-        
-        CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(lat, lng);
-        coords= realloc(coords, sizeof(CLLocationCoordinate2D)*(i+1));
-        if (CLLocationCoordinate2DIsValid(c)) {
-                                coords[i]= coord;
-                                *Coords2DLengthOut =i+1;
-                                }
-    }
-    return   coords;*/
-    
     
     NSUInteger read = 0, space = 10;
     CLLocationCoordinate2D *coords = malloc(sizeof(CLLocationCoordinate2D) * space);
