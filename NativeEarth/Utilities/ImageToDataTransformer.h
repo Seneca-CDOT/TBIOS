e500a3bea3ee94edbecbc47ac34dfb5e3b8d6238
Ladan Zahiroleslam <ladanzahir@gmail.com>
2011-11-09 14:06:39 -0500

    c

---
+++ NativeEarth/Utilities/ImageToDataTransformer.h
@@ -0,0 +1,19 @@
+//
+//  ImageToDataTransformer.h
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-11-09.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ImageToDataTransformer : NSValueTransformer {
+    
+}
+
+@end
+
+
+
