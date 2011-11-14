9c5c3641548bd4489a5c76aa9ac6e37487773e52
Ladan Zahiroleslam <ladanzahir@gmail.com>
2011-11-14 14:50:19 -0500

    c

---
+++ NativeEarth/WSGreeting.h
@@ -0,0 +1,31 @@
+//
+//  WSGreetings.h
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-07-28.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Greeting.h"
+@class WSContent;
+
+
+@interface WSGreeting : NSObject {
+    
+}
+@property(nonatomic, retain)NSString   * Language;
+
+@property (nonatomic, retain) NSString * ActorName;
+
+@property (nonatomic, retain) NSString * Phrase;
+@property (nonatomic, retain) NSString * PronounciationEnglish;
+@property (nonatomic, retain) NSString * PronounciationFrench;
+
+@property (nonatomic, retain) WSContent * Content;
+
+
+-(id) initWithDictionary:(NSDictionary *) greetingsDict;
+-(Greeting*) ToManagedGreeting:(NSManagedObjectContext*) context;
+
+@end
