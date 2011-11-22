2a6b6434e88695ab4f30a0f19ce8eaaeeffd4a6a
Ladan Zahiroleslam <ladanzahir@gmail.com>
2011-11-22 13:29:44 -0500

    c

---
+++ NativeEarth/Utilities/Utility+CLLocation.h
@@ -0,0 +1,17 @@
+//
+//  Utility+CLLocation.h
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-11-21.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface CLLocation (Utility)
+- (CLLocationDirection)directionToLocation:(CLLocation *)location;
+- (CLLocation *) destinationWithDistance:(CLLocationDistance) distance andDirection:(CLLocationDirection) direction;
+- (CLLocationDistance)distanceFromPathWithStartPoint:(CLLocation *)startPoint andEndPoint :(CLLocation*)endPoint;
+
+@end
