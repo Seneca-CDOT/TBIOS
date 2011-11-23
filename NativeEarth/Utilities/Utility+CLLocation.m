ad4a0908202406b3b7010571f5bb27fa56b8e344
Ladan Zahiroleslam <ladanzahir@gmail.com>
2011-11-23 11:00:21 -0500

    c

--- NativeEarth/Utilities/Utility+CLLocation.m
+++ NativeEarth/Utilities/Utility+CLLocation.m
@@ -8,8 +8,9 @@
 
 #import "Utility+CLLocation.h"
 
-#define  ER  6371 // km
+#define  ERM  6378137 //earth radious in m 
 #define RAD_TO_DEG(r) ((r) * (180 / M_PI))
+#define DEG_TO_RAD(d) (((d) * M_PI)/180)
 
 @implementation CLLocation (Utility)
 //http://forrst.com/posts/Direction_between_2_CLLocations-uAo
@@ -27,15 +28,19 @@
      Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
      var brng = Math.atan2(y, x).toDeg();
      */
-    CLLocationCoordinate2D coord1 = self.coordinate;
-    CLLocationCoordinate2D coord2 = location.coordinate;
+   
+    CLLocationDegrees lat1 = DEG_TO_RAD(self.coordinate.latitude);
+    CLLocationDegrees lng1 = DEG_TO_RAD(self.coordinate.longitude);
+    CLLocationDegrees lat2 = DEG_TO_RAD(location.coordinate.latitude);
+    CLLocationDegrees lng2 = DEG_TO_RAD(location.coordinate.longitude);
     
-    CLLocationDegrees deltaLong = coord2.longitude - coord1.longitude;
-    CLLocationDegrees yComponent = sin(deltaLong) * cos(coord2.latitude);
-    CLLocationDegrees xComponent = (cos(coord1.latitude) * sin(coord2.latitude)) - (sin(coord1.latitude) * cos(coord2.latitude) * cos(deltaLong));
+    CLLocationDegrees deltaLong = lng2 - lng1;
     
-    CLLocationDegrees radians = atan2(yComponent, xComponent);
-    CLLocationDegrees degrees = RAD_TO_DEG(radians) + 360;
+    CLLocationDegrees yComponent = sin(deltaLong) * cos(lat2);
+    CLLocationDegrees xComponent = (cos(lat1) * sin(lat2)) - (sin(lat1) * cos(lat2) * cos(deltaLong));
+    CLLocationDegrees brng = atan2(yComponent, xComponent);
+    
+    CLLocationDegrees degrees = RAD_TO_DEG(brng) + 360;
     
     return fmod(degrees, 360);
 }
@@ -49,10 +54,17 @@
      θ is the bearing (in radians, clockwise from north);
      d/R is the angular distance (in radians), where d is the distance travelled and R is the earth’s radius
      */
+    double ER = ERM/1000;
+    distance = distance/ER;
+    direction = DEG_TO_RAD(direction);
     
-    CLLocationDegrees lat2= asin(sin(self.coordinate.latitude)* cos(distance/ER) + cos(self.coordinate.latitude)* sin(distance/ER)*cos(direction));
-    CLLocationDegrees lng2 = self.coordinate.longitude + atan2(sin(direction)*sin(distance/ER)*cos(self.coordinate.latitude), cos(distance/ER)-sin(self.coordinate.latitude)*sin(lat2));
-    CLLocation * destination = [[CLLocation alloc] initWithLatitude:lat2 longitude:lng2];
+    CLLocationDegrees lat1= DEG_TO_RAD( self.coordinate.latitude);
+    CLLocationDegrees lng1= DEG_TO_RAD (self.coordinate.longitude);
+    
+    CLLocationDegrees lat2= asin(sin(lat1)* cos(distance/ER) + cos(lat1)* sin(distance/ER)*cos(direction));
+    CLLocationDegrees lng2 = lng1+ atan2(sin(direction)*sin(distance/ER)*cos(lat1), cos(distance/ER)-sin(lat1)*sin(lat2));
+    lng2 = fmod((lng2+3*M_PI), (2*M_PI))-M_PI;
+    CLLocation * destination = [[CLLocation alloc] initWithLatitude:RAD_TO_DEG(lat2) longitude:RAD_TO_DEG(lng2)];
     return destination;
 }
 
@@ -67,7 +79,7 @@
     JavaScript:	
     var dXt = Math.asin(Math.sin(d13/R)*Math.sin(brng13-brng12)) * R;
      */
-
+    double ER = ERM/1000;
     
     CLLocationDistance  distanceFromStartPoint = [startPoint distanceFromLocation:self]/1000; //d13
     CLLocationDirection directionFromStartPoint = [startPoint directionToLocation:self];//θ13
