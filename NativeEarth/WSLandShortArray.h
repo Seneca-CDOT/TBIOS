6abfaebc5dd300f4289e7de6409f1efb1bed89bc
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-03-06 16:10:07 -0500

    c

---
+++ NativeEarth/WSLandShortArray.h
@@ -0,0 +1,24 @@
+//
+//  LandShortArray.h
+//  NativeEarth
+//
+//  Created by Ladan Zahir on 11-08-10.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface LandShortDictionary : NSMutableDictionary <NSFetchedResultsControllerDelegate>{
+    NSFetchedResultsController * fetchedResultsControllerShortLands_;
+    NSManagedObjectContext * managedObjectContext_;
+}
+
+@property (nonatomic, retain) NSManagedObjectContext* managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController * fetchedResultsControllerShortLands;
+
+-(id)initWithManagedObjectContext:(NSManagedObjectContext *) context;
+
+
+
+@end
