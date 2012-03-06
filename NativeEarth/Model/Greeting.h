b7db919b41880d7d4770d0bfa3a1ac93e6854dc0
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-03-06 15:57:45 -0500

    c

--- NativeEarth/Model/Greeting.h
+++ NativeEarth/Model/Greeting.h
@@ -2,25 +2,30 @@
 //  Greeting.h
 //  NativeEarth
 //
-//  Created by Ladan Zahir on 11-12-04.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Created by Ladan Zahir on 12-03-06.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
-@class Content, Land;
 
 @interface Greeting : NSManagedObject {
 @private
 }
-@property (nonatomic, retain) NSString * PhraseEnglish;
-@property (nonatomic, retain) NSString * Language;
-@property (nonatomic, retain) NSString * PronounciationEnglish;
-@property (nonatomic, retain) NSString * PronounciationFrench;
-@property (nonatomic, retain) NSString * Type;
-@property (nonatomic, retain) NSString * PhraseFrench;
-@property (nonatomic, retain) Land * Land;
-@property (nonatomic, retain) Content * Content;
+@property (nonatomic, retain) NSString * HelloPronounciation;
+@property (nonatomic, retain) NSNumber * RowVersion;
+@property (nonatomic, retain) NSNumber * GreetingID;
+@property (nonatomic, retain) NSString * ThankYouPronounciation;
+@property (nonatomic, retain) NSString * WelcomePronounciation;
+@property (nonatomic, retain) NSString * ActorName;
+@property (nonatomic, retain) NSDate * RecordedOn;
+@property (nonatomic, retain) NSString * HelloMIMEType;
+@property (nonatomic, retain) NSString * WelcomeMIMEType;
+@property (nonatomic, retain) NSString * ThankYouMIMEType;
+@property (nonatomic, retain) NSData * Hello;
+@property (nonatomic, retain) NSData * Welcome;
+@property (nonatomic, retain) NSData * ThankYou;
+@property (nonatomic, retain) NSSet* Nations;
 
 @end
--- NativeEarth/Model/Greeting.m
+++ NativeEarth/Model/Greeting.m
@@ -2,25 +2,56 @@
 //  Greeting.m
 //  NativeEarth
 //
-//  Created by Ladan Zahir on 11-12-04.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Created by Ladan Zahir on 12-03-06.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import "Greeting.h"
-#import "Content.h"
-#import "Land.h"
 
 
 @implementation Greeting
-@dynamic PhraseEnglish;
-@dynamic Language;
-@dynamic PronounciationEnglish;
-@dynamic PronounciationFrench;
-@dynamic Type;
-@dynamic PhraseFrench;
-@dynamic Land;
-@dynamic Content;
+@dynamic HelloPronounciation;
+@dynamic RowVersion;
+@dynamic GreetingID;
+@dynamic ThankYouPronounciation;
+@dynamic WelcomePronounciation;
+@dynamic ActorName;
+@dynamic RecordedOn;
+@dynamic HelloMIMEType;
+@dynamic WelcomeMIMEType;
+@dynamic ThankYouMIMEType;
+@dynamic Hello;
+@dynamic Welcome;
+@dynamic ThankYou;
+@dynamic Nations;
 
+- (void)addNationsObject:(NSManagedObject *)value {    
+    NSSet *changedObjects = [[NSSet alloc] initWithObjects:&value count:1];
+    [self willChangeValueForKey:@"Nations" withSetMutation:NSKeyValueUnionSetMutation usingObjects:changedObjects];
+    [[self primitiveValueForKey:@"Nations"] addObject:value];
+    [self didChangeValueForKey:@"Nations" withSetMutation:NSKeyValueUnionSetMutation usingObjects:changedObjects];
+    [changedObjects release];
+}
+
+- (void)removeNationsObject:(NSManagedObject *)value {
+    NSSet *changedObjects = [[NSSet alloc] initWithObjects:&value count:1];
+    [self willChangeValueForKey:@"Nations" withSetMutation:NSKeyValueMinusSetMutation usingObjects:changedObjects];
+    [[self primitiveValueForKey:@"Nations"] removeObject:value];
+    [self didChangeValueForKey:@"Nations" withSetMutation:NSKeyValueMinusSetMutation usingObjects:changedObjects];
+    [changedObjects release];
+}
+
+- (void)addNations:(NSSet *)value {    
+    [self willChangeValueForKey:@"Nations" withSetMutation:NSKeyValueUnionSetMutation usingObjects:value];
+    [[self primitiveValueForKey:@"Nations"] unionSet:value];
+    [self didChangeValueForKey:@"Nations" withSetMutation:NSKeyValueUnionSetMutation usingObjects:value];
+}
+
+- (void)removeNations:(NSSet *)value {
+    [self willChangeValueForKey:@"Nations" withSetMutation:NSKeyValueMinusSetMutation usingObjects:value];
+    [[self primitiveValueForKey:@"Nations"] minusSet:value];
+    [self didChangeValueForKey:@"Nations" withSetMutation:NSKeyValueMinusSetMutation usingObjects:value];
+}
 
 
 @end
