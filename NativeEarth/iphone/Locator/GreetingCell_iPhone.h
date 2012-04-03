a24219ae043f354682d501648a4feafb2fd59234
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-04-03 12:50:45 -0400

    c

--- NativeEarth/iphone/Locator/GreetingCell_iPhone.h
+++ NativeEarth/iphone/Locator/GreetingCell_iPhone.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "Greeting.h"
+#import "greeting.h"
 #import <AVFoundation/AVFoundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 // cell identifier for this custom cell
