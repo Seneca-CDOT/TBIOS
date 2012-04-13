13362e37108f93ab8619ce3335afb1899ee80860
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-04-12 20:09:09 -0400

    c

--- NativeEarth/Utilities/Model.m
+++ NativeEarth/Utilities/Model.m
@@ -505,8 +505,8 @@ frcGreeting=frcGreeting_;
     NSArray * nearByNations = [allNations filteredArrayUsingPredicate:predicate];
    
         if ([nearByNations count]>=kSearchCountLimit || searchDistanceKM>kSearchDistanceLimit) {
-            latitude=0.0;
-            longitude=0.0;
+          //  latitude=0.0;
+           // longitude=0.0;
     
         }else {
             searchDistanceKM += kSearchExpantionParameter;
