1582d37385f8dbb3033aff88e652ed63e90ae182
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-04-18 11:29:59 -0400

    c

--- NativeEarth/ShortNation.m
+++ NativeEarth/ShortNation.m
@@ -16,16 +16,18 @@
     [super init];
     self.Number = [NSNumber numberWithInt:[[dict valueForKey:@"Number"]intValue] ];
     self.OfficialName = [[dict valueForKey:@"OfficialName"] description];
-    id version = [dict valueForKey:@"rowversion" ] ;
-    if([version isKindOfClass:[NSData class]]){//for local object
-        self.RowVersion = (NSData*)version ;   
-     
-    }else{//for remote object
-       
-    int len = sizeof(version)*[version count]; 
-        self.RowVersion=  [NSData dataWithBytes:version  length:len ] ;
-      
-    }
+    self.RowVersion =[[dict valueForKey:@"rowversion"] description];
+    
+ //   id version = [dict valueForKey:@"rowversion" ] ;
+//    if([version isKindOfClass:[NSData class]]){//for local object
+//        self.RowVersion = (NSData*)version ;   
+//     
+//    }else{//for remote object
+//       
+//    int len = sizeof(version)*[version count]; 
+//        self.RowVersion=  [NSData dataWithBytes:version  length:len ] ;
+//      
+//    }
     
     return self;
     }
