3a66e90d67786f49fcc50ea76d1016c8660ea58d
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-04-18 12:30:10 -0400

    c

--- NativeEarth/Model/Nation.h
+++ NativeEarth/Model/Nation.h
@@ -2,7 +2,7 @@
 //  Nation.h
 //  NativeEarth
 //
-//  Created by Ladan Zahir on 12-03-06.
+//  Created by Ladan Zahir on 12-04-18.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
@@ -14,43 +14,32 @@
 @interface Nation : NSManagedObject {
 @private
 }
+@property (nonatomic, retain) NSNumber * CenterLat;
+@property (nonatomic, retain) NSString * Phone;
 @property (nonatomic, retain) NSNumber * Number;
+@property (nonatomic, retain) NSString * CommunitySite;
+@property (nonatomic, retain) NSString * Address;
+@property (nonatomic, retain) NSNumber * CenterLong;
 @property (nonatomic, retain) NSString * rowversion;
 @property (nonatomic, retain) NSString * OfficialName;
-@property (nonatomic, retain) NSString * Address;
 @property (nonatomic, retain) NSString * PostCode;
-@property (nonatomic, retain) NSString * Phone;
-@property (nonatomic, retain) NSString * CommunitySite;
-@property (nonatomic, retain) NSNumber * CenterLat;
-@property (nonatomic, retain) NSNumber * CenterLong;
+@property (nonatomic, retain) NSSet* Maps;
 @property (nonatomic, retain) NSSet* Lands;
 @property (nonatomic, retain) Greeting * greeting;
 @property (nonatomic, retain) NSSet* PlannedVisits;
-@property (nonatomic, retain) NSSet* Maps;
-
-
-- (void)addLandsObject:(Land *)value ;
-
-- (void)removeLandsObject:(Land *)value;
-
-- (void)addLands:(NSSet *)value ;
-
-- (void)removeLands:(NSSet *)value;
-
-- (void)addPlannedVisitsObject:(PlannedVisit *)value ;
-
-- (void)removePlannedVisitsObject:(PlannedVisit *)value ;
-
-- (void)addPlannedVisits:(NSSet *)value ;
-
-- (void)removePlannedVisits:(NSSet *)value ;
 
 - (void)addMapsObject:(Map *)value ;
-
-- (void)removeMapsObject:(Map *)value;
-
+- (void)removeMapsObject:(Map *)value ;
 - (void)addMaps:(NSSet *)value ;
-
 - (void)removeMaps:(NSSet *)value ;
+- (void)addLandsObject:(Land *)value ;
+- (void)removeLandsObject:(Land *)value ;
+- (void)addLands:(NSSet *)value ;
+- (void)removeLands:(NSSet *)value ;
+- (void)addPlannedVisitsObject:(PlannedVisit *)value ;
+- (void)removePlannedVisitsObject:(PlannedVisit *)value ;
+- (void)addPlannedVisits:(NSSet *)value ;
+- (void)removePlannedVisits:(NSSet *)value ;
 
 @end
+
