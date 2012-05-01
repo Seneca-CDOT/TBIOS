95fb261d4194fb403043266ca7e32c9e3acaa735
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-04-30 22:41:17 -0400

    c

--- NativeEarth/iphone/NativeEarthAppDelegate_iPhone.m
+++ NativeEarth/iphone/NativeEarthAppDelegate_iPhone.m
@@ -22,9 +22,7 @@
     // Status bar
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackTranslucent];
  
-    //self.viewController.internetConnectionStatus = self.internetConnectionStatus;
-   // self.viewController.wifiConnectionStatus = self.wifiConnectionStatus;
-   // self.viewController.remoteHostStatus = self.wifiConnectionStatus;
+
     self.model = [[Model alloc] init];
 
     [self.window addSubview:self.viewController.view];
