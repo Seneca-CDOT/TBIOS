e37a07394cfe62351755ae424e6c0d2b3131ec5a
Ladan Zahiroleslam <ladanzahir@gmail.com>
2012-05-16 15:32:17 -0400

    c

--- NativeEarth/Utilities/ReverseGeocoder.h
+++ NativeEarth/Utilities/ReverseGeocoder.h
@@ -7,8 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Utility.h"
-#include <math.h>
 #import <MapKit/MapKit.h>
 @interface ReverseGeocoder : NSObject<NSFetchedResultsControllerDelegate> {
     @private
@@ -16,18 +14,8 @@
     double curLongitude;
 }
 
-@property(nonatomic,retain)  MKMapView * mapView; 
           
-//- (NSArray *) FindEstimatedMatchingLandsForCoordinateWithLat:(double)lat AndLng: (double) lng;
-//- (NSArray *) FindLandForCoordinateWithLat:(double)lat AndLng:(double) lng; 
-//- (NSArray *) FindNearbyLandsForCoordinateWithLat:(double)lat andLng:(double) lng;
+
 -(NSArray *) FindNearbyNationsForCoordinateWithLat:(double)lat andLng:(double) lng;
-//helper methods
-- (BOOL)PointWithLatitude: (double) lat AndLongitude:(double)lng BelongsToPolygonWithCoordinates:(NSArray*) coordinates;
-- (BOOL)PointWithLatitude:(double)lat AndLongitude:(double)lng IsAVerticeOfPolygonWithCoordinates:(NSArray *)coordinates;
-- (BOOL)PointWithLatitude:(double)lat AndLongitude:(double)lng IsOnASideOfMultyLineWithCoordinates:(NSArray *)coordinates;
--(double) DistanceOfPointCWithCLat: (double) cy AndCLng:(double) cx FromLineWithPointALat:(double) ay AndPointALng:(double) ax AndPointBLat:(double) by AndPointBLng:(double)bx;
--(double)KilometerDistanceOfPointAWithLat:(double)latA andLng:(double) lngA fromPointBWithLat:(double)latB andLng:(double)lngB;
--(double)DistanceOfPointCWithCLat:(double)cy AndCLng:(double)cx FromPolygonWithCoordinates:(NSArray *)coordinates;
--(double) RevisedDistanceOfPointC:(CLLocation*) C FromLineWithPointA:(CLLocation *) A AndPointB:(CLLocation*)B;
+
 @end
